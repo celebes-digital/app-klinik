e36f2f759d50d8a9d5f0b69f613c7331297cc57a
perdidev <perdidev@gmail.com>
2025-01-16 09:45:07 +0800

    core: add kunjungan model and migration

--- app/Models/Kunjungan.php
+++ app/Models/Kunjungan.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kunjungan extends Model
 {
-    //
+    protected $table        = 'kunjungan';
+    protected $primaryKey   = 'no_kunjungan';
+    protected $keyType      = 'string';
+    public $incrementing    = false;
+    public $timestamps      = false;
+
+    protected $fillable = [
+        'no_kunjungan',
+        'id_kunjungan',
+        'id_pasien',
+        'id_tenaga_medis',
+        'id_poli',
+        'keluhan_awal',
+        'tgl_kunjungan',
+    ];
 }
---
+++ database/migrations/2025_01_10_215024_create_kunjungans_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('kunjungan', function (Blueprint $table) {
+            $table->bigInteger('no_kunjungan')->primary();
+
+            $table->uuid('id_kunjungan');
+            $table->unsignedTinyInteger('id_poli');
+            $table->text('keluhan_awal');
+            $table->dateTime('tgl_kunjungan');
+
+            $table->foreignId('id_pasien')->constrained('pasien', 'id_pasien');
+            $table->foreignId('id_tenaga_medis')->constrained('tenaga_medis', 'id_tenaga_medis');
+            $table->foreign('id_poli')->references('id_poli')->on('poliklinik');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('kunjungan');
+    }
+};
