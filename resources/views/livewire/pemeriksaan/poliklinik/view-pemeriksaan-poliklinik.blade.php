3e90abbf503d8f9a4e231fc6352fbf47d0280d44
perdidev <perdidev@gmail.com>
2025-01-17 15:50:01 +0800

    feat: halaman anamnesa (keluhan awal)

--- resources/views/livewire/pemeriksaan/poliklinik/view-pemeriksaan-poliklinik.blade.php
+++ resources/views/livewire/pemeriksaan/poliklinik/view-pemeriksaan-poliklinik.blade.php
@@ -5,30 +5,10 @@
             wire:model="selectedTab"
             active-class="text-white rounded bg-primary"
             label-class="font-semibold"
-            label-div-class="p-2 rounded bg-primary/5"
+            label-div-class="p-2 mb-2 rounded bg-primary/5"
         >
             <x-tab name="anamnesis" label="Anamnesis">
-                {{-- <div
-                    x-data="{
-                        keluhanUtama: true,
-                        keluhanPenyerta: true,
-                    }" 
-                >
-                    <x-collapse x-model="keluhanUtama">
-                        <x-slot:heading>
-                            Keluhan Utama
-                        </x-slot:heading>
-                        <x-slot:content>
-                            <x-textarea
-                                label=""
-                                wire:model="bio"
-                                placeholder="Tuliskan keluhan utama pasien"
-                                hint="Max 1000 chars"
-                                rows="4"
-                            />
-                        </x-slot:content>
-                    </x-collapse>
-                </div> --}}
+                <livewire:pemeriksaan.poliklinik.create-anamnesis :$kunjungan />
             </x-tab>
             <x-tab name="klinis" label="Pemeriksaan Klinis">
                 <div>Klinis</div>
