44fdfa24074c7d1fac9213e9868f739fc27959ee
perdidev <perdidev@gmail.com>
2025-01-20 10:57:20 +0800

    feat: vite live reload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+
+import livewire from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: false,
+        }),
+
+        livewire({
+            refresh: [
+                ...refreshPaths,
+                "resources/css/app.css",
+                "app/Livewire/**/*.php",
+                "resources/views/**/*.blade.php",
+            ],
         }),
     ],
 });
