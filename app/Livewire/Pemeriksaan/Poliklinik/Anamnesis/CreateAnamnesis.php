7ffe0ab4332d9d06578290af86ceb4cc85f36f10
perdidev <perdidev@gmail.com>
2025-01-21 10:15:36 +0800

    feat: save anamnesis to db

--- app/Livewire/Pemeriksaan/Poliklinik/Anamnesis/CreateAnamnesis.php
+++ app/Livewire/Pemeriksaan/Poliklinik/Anamnesis/CreateAnamnesis.php
@@ -2,10 +2,48 @@
 
 namespace App\Livewire\Pemeriksaan\Poliklinik\Anamnesis;
 
+use App\Models\Anamnesis;
+use App\Models\Kunjungan;
 use Livewire\Component;
+use Mary\Traits\Toast;
 
 class CreateAnamnesis extends Component
 {
+    use Toast;
+
+    public Kunjungan $kunjungan;
+
+    public $keluhan_utama               = '';
+    public $keluhan_penyerta            = '';
+    public $riwayat_penyakit_sekarang   = '';
+    public $riwayat_penyakit_terdahulu  = '';
+    public $riwayat_penyakit_keluarga   = '';
+    public $riwayat_alergi              = '';
+    public $riwayat_pengobatan          = '';
+
+    public function mount()
+    {
+        $dataAnamnesis = Anamnesis::where('no_kunjungan', $this->kunjungan->no_kunjungan)->first();
+
+        if ($dataAnamnesis) {
+            $this->fill($dataAnamnesis);
+        }
+    }
+
+    public function save()
+    {
+        Anamnesis::updateOrCreate(
+            ['no_kunjungan' => $this->kunjungan->no_kunjungan],
+            [
+                ...$this->except('kunjungan'),
+                'no_kunjungan'  => $this->kunjungan->no_kunjungan,
+                'id_pasien'     => $this->kunjungan->id_pasien,
+            ]
+        );
+
+        $this->success('Berhasil menyimpan data Anamnesis');
+    }
+
     public function render()
     {
         return view('livewire.pemeriksaan.poliklinik.anamnesis.create-anamnesis');
--- resources/views/livewire/pemeriksaan/poliklinik/anamnesis/create-anamnesis.blade.php
+++ resources/views/livewire/pemeriksaan/poliklinik/anamnesis/create-anamnesis.blade.php
@@ -9,45 +9,52 @@
     <div class="space-y-6">
         <x-textarea
             label="Keluhan Utama"
-            wire:model="form.keluhan_utama"
+            wire:model="keluhan_utama"
             placeholder="Tuliskan keluhan utama pasien"
             rows="2"
         />
         <x-textarea
             label="Keluhan Penyerta"
-            wire:model="form.keluhan_penyerta"
+            wire:model="keluhan_penyerta"
             placeholder="Tuliskan keluhan penyerta pasien"
             rows="2"
         />
         <x-textarea
             label="Riwayat Penyakit Sekarang"
-            wire:model="form.riwayat_penyakit_sekarang"
+            wire:model="riwayat_penyakit_sekarang"
             placeholder="Tuliskan riwayat penyakit sekarang pasien"
             rows="2"
         />
         <x-textarea
             label="Riwayat Penyakit Terdahulu"
-            wire:model="form.riwayat_penyakit_terdahulu"
+            wire:model="riwayat_penyakit_terdahulu"
             placeholder="Tuliskan riwayat penyakit terdahulu pasien"
             rows="2"
         />
         <x-textarea
             label="Riwayat Penyakit Keluarga"
-            wire:model="form.riwayat_penyakit_keluarga"
+            wire:model="riwayat_penyakit_keluarga"
             placeholder="Tuliskan riwayat penyakit keluarga pasien"
             rows="2"
         />
         <x-textarea
             label="Riwayat Alergi"
-            wire:model="form.riwayat_alergi"
+            wire:model="riwayat_alergi"
             placeholder="Tuliskan riwayat alergi pasien"
             rows="2"
         />
         <x-textarea
             label="Riwayat Pengobatan"
-            wire:model="form.riwayat_pengobatan"
+            wire:model="riwayat_pengobatan"
             placeholder="Tuliskan riwayat pengobatan pasien"
             rows="2"
+        />
+    </div>
+    <div>
+        <x-button 
+            label="Simpan" 
+            class="btn-primary" 
+            wire:click="save" 
         />
     </div>
 </div>
