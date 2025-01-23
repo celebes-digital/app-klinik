8905f3c7fdd9c931006b57da6316b2967beec17d
perdidev <perdidev@gmail.com>
2025-01-23 11:13:48 +0800

    fix: update trait wilayah indonesia to array (bug happen because livewire doesnt support collection)

--- app/Traits/WilayahIndonesia.php
+++ app/Traits/WilayahIndonesia.php
@@ -2,66 +2,82 @@
 
 namespace App\Traits;
 
-use App\SatuSehat\Terminologi\KodeWilayah;
+use App\Models\KodeWilayah;
+use Illuminate\Support\Facades\Log;
 
 trait WilayahIndonesia
 {
-    protected ?KodeWilayah $kodeWilayah = null;
-
     public $filteredProvinsi    = [];
     public $filteredKabupaten   = [];
     public $filteredKecamatan   = [];
     public $filteredKelurahan   = [];
 
-    public function bootWilayahIndonesia()
-    {
-        $this->kodeWilayah = new KodeWilayah();
-    }
-
     public function mountWilayahIndonesia()
     {
-        $res = $this->kodeWilayah->getProvinsi();
-        $this->filteredProvinsi = $res;
-    }
+        Log::info('Mounting Wilayah Indonesia');
+        $this->filteredProvinsi = $this->getProvinsi();
 
-    public function setDataWilayah($kodeProv, $kodeKab, $kodeKec)
-    {
-        $this->updatedProvinsi($kodeProv);
-        $this->updatedKabupaten($kodeKab);
-        $this->updatedKecamatan($kodeKec);
-        $this->form->provinsi = $kodeProv;
-        $this->form->kabupaten = $kodeKab;
-        $this->form->kecamatan = $kodeKec;
+        if ($this->form->provinsi) {
+            $this->filteredKabupaten = $this->getKabupaten($this->form->provinsi);
+        }
+
+        if ($this->form->kabupaten) {
+            $this->filteredKecamatan = $this->getKecamatan($this->form->kabupaten);
+        }
+
+        if ($this->form->kecamatan) {
+            $this->filteredKelurahan = $this->getKelurahan($this->form->kecamatan);
+        }
     }
 
     public function updatedWilayahIndonesia($key, $value)
     {
-        if ($key == 'provinsi' || $key == 'form.provinsi')
+        if ($key == 'form.provinsi')
             $this->updatedProvinsi($value);
-        else if ($key == 'kabupaten' || $key == 'form.kabupaten')
+        else if ($key == 'form.kabupaten')
             $this->updatedKabupaten($value);
-        else if ($key == 'kecamatan' || $key == 'form.kecamatan')
+        else if ($key == 'form.kecamatan')
             $this->updatedKecamatan($value);
     }
 
     public function updatedProvinsi($value)
     {
-        $this->filteredKabupaten = $this->kodeWilayah->getKabupaten($value);
-        $this->reset('form.kabupaten', 'filteredKecamatan', 'filteredKelurahan');
-        $this->filteredKecamatan = [];
-        $this->filteredKelurahan = [];
+        $this->filteredKabupaten    = $this->getKabupaten($value);
+        $this->reset(['form.kabupaten', 'form.kecamatan', 'form.kelurahan']);
+        $this->filteredKecamatan    = [];
+        $this->filteredKelurahan    = [];
     }
 
     public function updatedKabupaten($value)
     {
-        $this->reset('filteredKecamatan', 'filteredKelurahan');
-        $this->filteredKecamatan = $this->kodeWilayah->getKecamatan($value);
+        $this->filteredKecamatan = $this->getKecamatan($value);
+        $this->reset(['form.kecamatan', 'form.kelurahan']);
         $this->filteredKelurahan = [];
     }
 
     public function updatedKecamatan($value)
     {
-        $this->reset('filteredKelurahan');
-        $this->filteredKelurahan = $this->kodeWilayah->getKelurahan($value);
+        $this->filteredKelurahan = $this->getKelurahan($value);
+        $this->reset('form.kelurahan');
+    }
+
+    public function getProvinsi()
+    {
+        return KodeWilayah::select('kode_provinsi', 'nama_provinsi')->distinct()->get()->toArray();
+    }
+
+    public function getKabupaten($kodeProvinsi)
+    {
+        return KodeWilayah::select('kode_kabupaten', 'nama_kabupaten')->where('kode_provinsi', $kodeProvinsi)->distinct()->get()->toArray();
+    }
+
+    public function getKecamatan($kodeKabupaten)
+    {
+        return KodeWilayah::select('kode_kecamatan', 'nama_kecamatan')->where('kode_kabupaten', $kodeKabupaten)->distinct()->get()->toArray();
+    }
+
+    public function getKelurahan($kodeKecamatan)
+    {
+        return KodeWilayah::select('kode_kelurahan', 'nama_kelurahan')->where('kode_kecamatan', $kodeKecamatan)->distinct()->get()->toArray();
     }
 }
